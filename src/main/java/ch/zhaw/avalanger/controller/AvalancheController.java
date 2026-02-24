31aaa8383ff4b71491537f569ad983e2c46fabc3
iacopo <foniiac1@students.zhaw.ch>
2026-02-24 08:53:14 +0100

    avalangeController

---
+++ src/main/java/ch/zhaw/avalanger/controller/AvalancheController.java
@@ -0,0 +1,16 @@
+package ch.zhaw.avalanger.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/avalange")
+public class AvalancheController {
+
+    @GetMapping("")
+    public String getAllAvalange() {
+        return "Get all avalange";
+    }
+
+}
