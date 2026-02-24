06bb7b48fa1e7b14c2de45a52de9d72f482b23bb
iacopo <foniiac1@students.zhaw.ch>
2026-02-24 09:22:42 +0100

    addlombok

--- src/main/java/ch/zhaw/avalanger/controller/AvalancheController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalancheController.java
@@ -3,8 +3,13 @@ package ch.zhaw.avalanger.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.avalanger.model.Avalange;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/avalange")
@@ -14,6 +19,13 @@ public class AvalancheController {
     public String getAllAvalange(@PathVariable(required = false) String country,
             @RequestParam(required = false) String state) {
         return "Get all avalange for country: " + country + " and state: " + state;
+    }
+
+    @PostMapping("")
+    public String postMethodName(@RequestBody Avalange avalange) {
+
+        return "Avalanche created: " + avalange.getCountry() + ", " + avalange.getState() + ", "
+                + avalange.getDescription();
     }
 
 }
