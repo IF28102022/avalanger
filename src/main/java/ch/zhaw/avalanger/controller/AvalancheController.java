0cc578d9e7cf491f3ca0e36fb1762942dca2d9f8
iacopo <foniiac1@students.zhaw.ch>
2026-02-24 09:05:59 +0100

    Added country and state

--- src/main/java/ch/zhaw/avalanger/controller/AvalancheController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalancheController.java
@@ -1,16 +1,19 @@
 package ch.zhaw.avalanger.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/avalange")
 public class AvalancheController {
 
-    @GetMapping("")
-    public String getAllAvalange() {
-        return "Get all avalange";
+    @GetMapping(value = { "", "/{country}" })
+    public String getAllAvalange(@PathVariable(required = false) String country,
+            @RequestParam(required = false) String state) {
+        return "Get all avalange for country: " + country + " and state: " + state;
     }
 
 }
